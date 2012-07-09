ef6cdc202d8ad8671d32fd34843142c437dc40b3
Chad Dibble <chad@chadthebad.com>
2012-07-09 15:45:11 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groundhog::Application.config.secret_token = 'e8d6d9489bbc8fb44754878613d9895b01631c504245690e81735e9c5ee8e17dff4378404637fcae7721f8ccf1968bb879dd1740844d509724972fe26270e1bf'
