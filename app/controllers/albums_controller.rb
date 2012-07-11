4206d08c054bcfe401d90ef04f0866040a07ec91
Chad Dibble <chad@chadthebad.com>
2012-07-11 15:53:54 -0700

    Finished controllers - added views

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,3 +1,45 @@
-
 class AlbumsController < ApplicationController
+
+  def index
+    @albums = Album.all
+  end
+  
+  def show
+    @album = Album.find(params[:id])
+  end
+  
+  def new
+    @album = Album.new
+  end
+  
+  def edit
+    @album = Album.find(params[:id])
+  end
+  
+  def create
+    @album = Album.new(params[:album])
+    
+    if @album.save
+      redirect_to @album, notice: 'Album created successfully'
+    else
+      render action: "new"
+    end
+  end
+  
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update_attributes(params[:album])
+      redirect_to @album, notice: 'Album was successfully updated.' 
+    else
+      render action: "edit" 
+    end
+  end
+  
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to albums_url 
+  end
+
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,45 @@
-
 class PhotosController < ApplicationController
+
+  def index
+    @photos = Photo.all
+  end
+  
+  def show
+    @photo = Photo.find(params[:id])
+  end
+  
+  def new
+    @photo = Photo.new album_id: params[:album_id]
+  end
+  
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+  
+  def create
+    @photo = Photo.new(params[:photo])
+    
+    if @photo.save
+      redirect_to @photo.album, notice: 'Photo was successfully uploaded.'
+    else
+      render action: "new"
+    end
+  end
+  
+  def update
+    @photo = Photo.find(params[:id])
+    
+    if @photo.update_attributes(params[:photo])
+      redirect_to @photo, notice: 'Photo was successfully updated'
+    else
+      render action: "edit"
+    end
+  end
+  
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to photos_url
+  end
+
 end
